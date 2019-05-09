124392e8114e40f9cf076e4c3b8fcc2ce3bc8ffb
zhangyubo <zhangyubo@gouuse.cn>
2019-05-09 15:09:13 +0800

    rpc优化

--- src/php/thrift/CeCd/Sdk/Core/RpcArrayException.php
+++ src/php/thrift/CeCd/Sdk/Core/RpcArrayException.php
@@ -8,6 +8,6 @@ class RpcArrayException extends \Exception
 {
     public function __construct(array $message = [], \Exception $previous = null, $code = 0)
     {
-        parent::__construc(json_encode($message), $code, $previous);
+        parent::__construct(json_encode($message), $code, $previous);
     }
 }
