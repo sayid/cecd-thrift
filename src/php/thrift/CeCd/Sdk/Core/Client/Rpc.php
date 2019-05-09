3e8ed13fa9f5e1eb489888491322cf9b7a8b9631
zhangyubo <zhangyubo@gouuse.cn>
2019-05-09 14:54:50 +0800

    rpc优化

--- src/php/thrift/CeCd/Sdk/Core/Client/Rpc.php
+++ src/php/thrift/CeCd/Sdk/Core/Client/Rpc.php
@@ -15,7 +15,7 @@ use Thrift\Transport\TSocket;
 use Thrift\Transport\TFramedTransport;
 use Thrift\Exception\TException;
 use Thrift\CeCd\Sdk\RpcServiceClient;
-use Thrift\CeCd\Sdk\Core\RpcException;
+use Thrift\CeCd\Sdk\Core\RpcArrayException;
 
 class Rpc
 {
@@ -55,7 +55,7 @@ class Rpc
                     $res->data = ceRpcDecode($res->data);
                     return $res->data;
                 } elseif ($res->code) {
-                    throw new RpcException(['code' => $res->code, 'msg' => $res->msg , 'exception' => $res->ex]);
+                    throw new RpcArrayException(['code' => $res->code, 'msg' => $res->msg , 'exception' => $res->ex]);
                 }
             } catch (TException $tx) {
                 if ($i == 1) {
