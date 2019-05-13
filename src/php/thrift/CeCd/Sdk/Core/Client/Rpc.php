2902428d8f8589771d91e32e6d4a0a5e4c16d893
zhangyubo <zhangyubo@gouuse.cn>
2019-05-13 10:44:16 +0800

    rpc优化

--- src/php/thrift/CeCd/Sdk/Core/Client/Rpc.php
+++ src/php/thrift/CeCd/Sdk/Core/Client/Rpc.php
@@ -40,30 +40,12 @@ class Rpc
         $this->extraData = $extraData;
     }
 
-    public function preExtraData()
-    {
-        $GOUUSE_MEMBER_INFO = getGouuseCore()->AuthLib->getMember();
-        $GOUUSE_COMPANY_INFO = getGouuseCore()->AuthLib->getCompany();
-        $member_keys = ['member_id', 'company_id', 'member_name','superior_id','name_initial','name_initial_all','role_group','sex','email','mobile','position_id','phone','department_id','status','work_number','join_time','registr_time','become_regular_time','time_delete','first_work_time', 'avatar'];
-        $company_keys = ['company_id', 'company_name', 'company_short', 'company_no', 'logo', 'max_member', 'industry_id', 'admin_id'];
-        return [
-            'member_id' => $GOUUSE_MEMBER_INFO['member_id'] ?? 0,
-            'company_id' => $GOUUSE_MEMBER_INFO['company_id'] ?? 0,
-            'member_info' => $GOUUSE_MEMBER_INFO,
-            'company_info' => $GOUUSE_COMPANY_INFO,
-            'from_service_id' => env('SERVICE_ID')//来自哪个服务
-        ];
-    }
-
     public function callRpc($classname, $method, $args = [])
     {
         $host = $this->rpcModule->getHost();
         $port = $this->rpcModule->getPort();
-        if ($this->extraData) {
-            $extra = ceRpcEncode($this->extraData);
-        } else {
-            $extra = $this->preExtraData();
-        }
+
+        $extra = ceRpcEncode($this->extraData);
 
         $args = ceRpcEncode($args);
         $RpcPools = RpcPools::getInstance();
