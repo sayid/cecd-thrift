5e0b0dcde5619128d9e72b3ea20d1905039281be
zhangyubo <zhangyubo@gouuse.cn>
2019-06-06 17:42:40 +0800

    rpc优化 增加服务基类

--- src/java/src/main/java/com/cecd/sdk/examples/usercenter/UserCenter.java
+++ src/java/src/main/java/com/cecd/sdk/examples/usercenter/UserCenter.java
@@ -9,10 +9,10 @@ public class UserCenter extends RpcModuleAbstract {
 
     public UserCenter() {
         //初始化后放入工厂类中
-        this.setInterceptor(RpcClientIntercepterTest.class);
+        this.setInterceptor(new RpcClientIntercepterTest());
         this.setServiceId(1005);
         this.setServiceName("user-center");
-        this.setLang("java");
+        this.setLang("php");
         RpcFactory.addService(this);
     }
 
