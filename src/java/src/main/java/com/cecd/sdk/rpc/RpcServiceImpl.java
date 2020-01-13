9d3bb7379ef9e123a1b4e4f49a0777faff7b2fa8
zhangyubo <zhangyubo@gouuse.cn>
2020-01-13 14:40:24 +0800

    升级rpc服务

--- src/java/src/main/java/com/cecd/sdk/rpc/RpcServiceImpl.java
+++ src/java/src/main/java/com/cecd/sdk/rpc/RpcServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Array;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
