5e5e15997a8546834d3e47b2f803d557b9e190d6
zhangyubo <zhangyubo@gouuse.cn>
2021-04-29 09:48:29 +0800

    youhuarpc

--- src/php/example/Product/Services/CategoryServiceImpl.php
+++ src/php/example/Product/Services/CategoryServiceImpl.php
@@ -11,6 +11,7 @@ use Thrift\CeCd\Sdk\Core\Services\RpcInterface;
  * @package Cecd\Sdk\Rpc\Product\Services
  * @classpath(com.cecd.project.product.service.CategoryServiceImpl)
  */
+#[ServerClass(com.cecd.project.product.service.CategoryServiceImpl)]
 interface CategoryServiceImpl extends RpcInterface
 {
     public function getList(int $offset, int $limit) : array;
@@ -19,5 +20,6 @@ interface CategoryServiceImpl extends RpcInterface
      * @param object $arg
      * @return array
      */
+    #[ServerArgType(object)]
     public function testMap(object $arg) : object;
 }
